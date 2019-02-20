3aff678060d1ad468772ff75b56f5e91bc7ec13f
ptrdtznr <peter.detzner@iml.fraunhofer.de>
2019-02-20 18:59:19 +0100

    Adding of a token to the SAN Endpoint. The Data will be forwarderd to the task-thread through the dictQueue-dispatcher

--- endpoints/san.py
+++ endpoints/san.py
@@ -9,12 +9,13 @@ import globals
 
 san_bp = Blueprint('sanEndpoint', __name__)
 
+dictQueue = globals.sanDictQueue
 
-
-@san_bp.route('', methods=['GET', 'POST']) 
-def sanEndPoint():
+@san_bp.route('/<token>', methods=['GET', 'POST']) 
+def sanEndPoint(token):
     """Renders the home page.""" 
-    if request.json: 
+    if request.json:  
+        dictQueue.putData(token, "funzt")
         jsonReq = request.json
         if(globals.FI_SUB_ID in jsonReq and globals.FI_DATA in jsonReq):
             subId =jsonReq[globals.FI_SUB_ID] 
