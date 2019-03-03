b5396ac160ddf019ac0560ccb64db3eca0b4b57e
ptrdtznr <peter.detzner@iml.fraunhofer.de>
2019-03-03 21:01:12 +0100

    global OCB Handler added

--- globals.py
+++ globals.py
@@ -4,6 +4,10 @@ import threading
 
 from dictQueue import DictQueue
 
+from contextbrokerhandler import ContextBrokerHandler
+from configParser import Config
+
+
 taskQueue = Queue.Queue()
 sanDictQueue = DictQueue()
 sanQueue = Queue.Queue()
@@ -14,3 +18,14 @@ subscriptionDict = {}
 
 FI_SUB_ID = "subscriptionId"
 FI_DATA = "data"
+
+ocbHandler = None
+CONFIG_FILE = "./fiware_config.ini"
+parsedConfigFile = Config(CONFIG_FILE)
+# globals.initOcbHandler(parsedConfigFile.getFiwareServerAddress())
+ocbHandler = ContextBrokerHandler(parsedConfigFile.getFiwareServerAddress())
+
+# def initOcbHandler(_address):
+#     global ocbHandler
+#     ocbHandler = ContextBrokerHandler(_address)
+#     print "help"
