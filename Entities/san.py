d3bd3881eb939b95d404b9cb8ecd3381b5de1f74
ptrdtznr <peter.detzner@iml.fraunhofer.de>
2019-03-07 14:57:11 +0100

    SensorAgent Entity

--- Entities/san.py
+++ Entities/san.py
@@ -3,13 +3,28 @@ from enum import IntEnum
 
 from entity import FiwareEntity
 
-class San(FiwareEntity): 
-    def __init__(self): 
-        self.taskOrder = 0 
-        self.taskId = 0
+class SensorData(object):
+   def __init__(self, **entries):
+      self.manufacturer = ""
+      self.measurementType = ""
+      self.readings = []
+      self.sensorId = ""
+      self.sensorType = ""
+      self.__dict__.update(entries)  # Insert values from given dict
 
-    @classmethod
-    def getEntity(cls):
-        print cls.Type()
-        print cls.Id()
-        return { "id" : "SAN_demo" , "type" : "SensorAgent" }
+class SensorAgent(FiwareEntity):
+    def __init__(self):
+        FiwareEntity.__init__(self)
+        self.sensorData = [] # List of SensorData
+        self.type = "SAN_demo"
+        self.modifiedTime = "" # ISO8601
+
+# class San(FiwareEntity): 
+#     def __init__(self):  
+#         pass
+    
+#     @classmethod
+#     def getEntity(cls):
+#         print cls.Type()
+#         print cls.Id()
+#         return { "id" : "SAN_demo" , "type" : "SensorAgent" }
