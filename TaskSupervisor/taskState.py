22d3bcbac6b800cd9e5ae554330bcbe3ae99c4e0
ptrdtznr <peter.detzner@iml.fraunhofer.de>
2019-04-04 16:19:39 +0200

    Clean up the taskState

--- TaskSupervisor/taskState.py
+++ TaskSupervisor/taskState.py
@@ -1,8 +1,28 @@
 from enum import IntEnum
+from Entities.entity import FiwareEntity
 
-  
+class TaskState(FiwareEntity):
+
+    def __init__(self, _task):
+        if(not isinstance(_task, Task)):
+            raise Exception("TypeMissmatch")
+        FiwareEntity.__init__(self, id=_task.id)
+        self.name = _task.taskName
+        self.state = State.Idle
+        self.taskId = _task.id
+        self.taskManagerId = _task.taskManagerId
+        # self.taskSpecUuid = None
+        self.errorMessage = ""
 
 
+class State():
+    Idle = 0
+    Running = 1
+    Waiting = 2
+    Active = 3
+    Finished = 4
+    Aborted = 5
+    Error = 6
 
 class UserAction():
     Idle = 0
