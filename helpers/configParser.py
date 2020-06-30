a280135420e72129c121d8db584452100f218826
ptrdtznr <peter.detzner@iml.fraunhofer.de>
2020-06-30 13:09:50 +0200

    Check if file config file exists

--- helpers/configParser.py
+++ helpers/configParser.py
@@ -1,32 +1,40 @@
-import ConfigParser
 import io
 import sys
-
+import os.path
+from os import path
  
+import ConfigParser
 
 class Config(object):
     """description of class"""
     def __init__(self, _fileName):
         
         CONFIG_FILE = _fileName
-        with open(CONFIG_FILE,'r+') as f:
-            sample_config = f.read()
-        config = ConfigParser.RawConfigParser(allow_no_value=True)
-        config.readfp(io.BytesIO(sample_config))
-        self.CB_HOST=config.get('contextbroker', 'host')
-        self.CB_PORT=config.get('contextbroker', 'port')
-        #self.CB_FIWARE_SERVICE=config.get('contextbroker', 'fiware_service')
-        self.CB_FIWARE_SERVICEPATH = "/Sensors"
-        self.CB_URL = "http://"+self.CB_HOST+":"+self.CB_PORT
-        self.TASKPLANNER_PORT = int(config.get('taskplanner', 'port'))
-        self.TASKPLANNER_HOST = config.get('taskplanner', 'host')
+    
+        if(path.exists(_fileName)):
+            with open(CONFIG_FILE,'r+') as f:
+                sample_config = f.read()
+            config = ConfigParser.RawConfigParser(allow_no_value=True)
+            config.readfp(io.BytesIO(sample_config))
+            self.CB_HOST=config.get('contextbroker', 'host')
+            self.CB_PORT=config.get('contextbroker', 'port')
+            #self.CB_FIWARE_SERVICE=config.get('contextbroker', 'fiware_service')
+            self.CB_FIWARE_SERVICEPATH = "/Sensors"
+            self.CB_URL = "http://"+self.CB_HOST+":"+self.CB_PORT
+            self.TASKPLANNER_PORT = int(config.get('taskplanner', 'port'))
+            self.TASKPLANNER_HOST = config.get('taskplanner', 'host')
+            
+            #self.robot_id = config.get('RAN', 'robot_id')
+            self.FLASK_HOST = "0.0.0.0"
+            
+            self.robots = self._trimArray(config.get("robots", "ids").split(","))
+            self.robotTypes = self._trimArray(config.get("robots", "types").split(","))
+            self.robotNames = self._trimArray(config.get("robots", "names").split(","))
+        else:
+            print("ERROR OPENING CONFIG FILE " + _fileName)
+            raise OSError("Config File does not exists")
+      
         
-        #self.robot_id = config.get('RAN', 'robot_id')
-        self.FLASK_HOST = config.get('flask', 'host')
-        
-        self.robots = self._trimArray(config.get("robots", "ids").split(","))
-        self.robotTypes = self._trimArray(config.get("robots", "types").split(","))
-        self.robotNames = self._trimArray(config.get("robots", "names").split(","))
           
     def getTaskPlannerAddress(self):
         return "http://"+ self.TASKPLANNER_HOST + ":" + str(self.TASKPLANNER_PORT)
