ce443d43b2ecc4752a1da3542bea960d5831c8eb
ptrdtznr <peter.detzner@iml.fraunhofer.de>
2020-06-30 07:16:25 +0200

    print-statement in brackets

--- scripts/killTs.py
+++ scripts/killTs.py
@@ -2,12 +2,12 @@ import subprocess, signal
 import os
 p = subprocess.Popen(['ps', '-ef'], stdout=subprocess.PIPE)
 out, err = p.communicate()
-print "Start Trying to kill all processes of \'runserver\'"
+print ("Start Trying to kill all processes of \'runserver\'")
 for line in out.splitlines():
     if 'runserver' in line:
         pid = int(line.split()[1])
-        print "killing process with id: " + str(pid)
+        print ("killing process with id: " + str(pid))
         os.kill(pid, signal.SIGKILL)
         #print line
-print "End of process kill runserver"
+print ("End of process kill runserver")
         
