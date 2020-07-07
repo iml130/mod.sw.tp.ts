c5cec872bd42df4d41ffae256708ce4ea19439d9
ptrdtznr <peter.detzner@iml.fraunhofer.de>
2020-07-07 11:54:27 +0200

    Update to 3.1.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from flask import Flask
 app = Flask(__name__)
 
 VERSION = """Fraunhofer IML<br />
-OPIL TaskPlanner v3.1.4 - 02.07.2020 (#06041986)<br />
+OPIL TaskPlanner v3.1.5 - 07.07.2020 (#06041986)<br />
 Running...<br />
 """
 import views 
