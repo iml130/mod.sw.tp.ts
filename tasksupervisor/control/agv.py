24a7a53f4f10d6c898265e50171f9b4b4b2e2833
Peter Detzner <peter.detzner@iml.fraunhofer.de>
2020-10-27 10:33:40 +0100

    Update AGV design

--- tasksupervisor/control/agv.py
+++ tasksupervisor/control/agv.py
@@ -1,16 +1,3 @@
-class Location():
-    def __init__(self):
-        self.coord_x = 0
-        self.coord_y = 0
-        self.theta = 0
-
-class WorkingQueue():
-    def __init__(self):
-        self.tasks_upcoming = 0
-        self.tasks_done = 0
-        self.busy_time = 0
-
-
 class AGV():
     def __init__(self, _id, _name, _type):
         self.id = _id
@@ -18,3 +5,34 @@ class AGV():
         self.type = _type
         self.location = Location()
         self.working_queue = WorkingQueue()
+    
+    def set_control(self, control):
+        self.control = control
+    
+    def get_control(self):
+        return self.control
+
+class Location():
+    def __init__(self):
+        self.coord_x = 0
+        self.coord_y = 0
+        self.theta = 0
+
+    def set_location(self, location: Location):
+        # assert to check if location is current location
+        self.coord_x = location.coord_x
+        self.coord_y = location.coord_y
+        self.theta = location.theta
+
+    def update_location(self, x, y, theta):
+        self.coord_x = x
+        self.coord_y = y
+        self.theta = theta
+
+
+class WorkingQueue():
+    def __init__(self):
+        self.tasks_upcoming = 0
+        self.tasks_done = 0
+        self.busy_time = 0
+
