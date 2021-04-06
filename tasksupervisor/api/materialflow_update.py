5f0893754da0fda36b6c86361423b521e0928181
Maximilian Hörstrup <maximilian.hoerstrup@iml.fraunhofer.de>
2021-04-06 09:16:02 +0200

    Add missing entity classes in api folder
    All entities are now complete in the api folder

---
+++ tasksupervisor/api/materialflow_update.py
@@ -0,0 +1,14 @@
+import uuid
+
+from tasksupervisor.entities.entity import FiwareEntity
+from tasksupervisor.helpers.utc import get_utc_time
+
+class MaterialflowUpdate():
+    """ Representation of a materialflow update """
+    def __init__(self, materialflow):
+        self.id = uuid.uuid4() 
+        self.task_manager_name = materialflow.task_manager_name
+        self.transport_order_list = materialflow.transport_order_list
+        self.ref_owner_id = materialflow.ref_owner_id
+        self.time = get_utc_time()
+        self.broker_ref_id = materialflow.broker_ref_id
---
+++ tasksupervisor/api/tasksupervisor_info.py
@@ -0,0 +1,19 @@
+import uuid
+
+class TaskSupervisorInfo():
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.used_materialflows = []
+        self.number_of_materialflows = len(self.used_materialflows)
+        self.message = ""
+        self.broker_ref_id = ""
+
+    def appendMaterialflow(self, _id):
+        if _id not in self.used_materialflows:
+            self.used_materialflows.append(_id)
+            self.number_of_materialflows = len(self.used_materialflows)
+
+    def removeMaterialFlow(self, _id):
+        if _id in self.used_materialflows == True:
+            self.used_materialflows.remove(_id)
+            self.number_of_materialflows = len(self.used_materialflows)
