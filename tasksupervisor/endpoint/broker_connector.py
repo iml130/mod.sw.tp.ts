2852db14aeccd82c7982936cbc8966b0e0f9192d
Maximilian Hörstrup <maximilian.hoerstrup@iml.fraunhofer.de>
2021-04-06 09:30:15 +0200

    Rework code and add missing methods
    Add methods for create, delete and update in both classes.
    Remove main method for testing
    Implement shutdown method in both classes.

--- tasksupervisor/endpoint/broker_connector.py
+++ tasksupervisor/endpoint/broker_connector.py
@@ -1,38 +1,79 @@
 from tasksupervisor import my_globals
 
+class BrokerException(Exception):
+    def __init__(self, status, message, *args, **kwargs):
+        super().__init__(status, message, *args, **kwargs)
+
 class BrokerConnector:
     """
         Layer between Broker Interfaces and Supervisor.
         Manages multiple Interface instances and the correct distribution of messages
     """
-
-    def __init__(self, ):
-        self.interfaces = []
-        self.interface_mf_ids_dict = dict()
-        self.mf_ids_interface_dict = dict()
+    
+    def __init__(self, task_supervisor):
+        self.interfaces = {}
+        self.task_supervisor = task_supervisor
+        self.sensor_id_to_ids_dict = dict()
 
     def register_interface(self, interface):
-        self.interfaces.append(interface)
-        self.interface_mf_ids_dict[interface] = []
+        self.interfaces[interface.broker_id] = interface
 
-    def subscribe_specific(self, topic):
-        pass
+    def unregister_interface(self, interface):
+        if interface.broker_id in self.interfaces:
+            del self.interfaces[interface.broker_id]
 
-    def subscribe_generic(self, topic):
-        for interface in self.interfaces:
-            interface.subscribe(topic)
+    def subscribe_to_specific(self, topic, broker_id, opt_data=None, generic=False):
+        class_name = str(topic.__class__.__name__)
+        if class_name == "SensorAgent":
+            transport_order_id = opt_data.to_id
+            self.sensor_id_to_ids_dict[topic.id] = []
+            self.sensor_id_to_ids_dict[topic.id].append(transport_order_id)
+
+        interface = self.interfaces[broker_id]
+        return interface.subscribe(topic, opt_data=opt_data, generic=generic)
+
+    def subscribe_to_all(self, topic, opt_data=None, generic=False):
+        for interface in self.interfaces.values():
+            interface.subscribe(topic, opt_data=opt_data, generic=generic)
 
     def retreive(self, data, interface):
         class_name = str(data.__class__.__name__)
         if class_name == "Materialflow":
-            self.interface_mf_ids_dict[interface].append(data.id)
             my_globals.taskQueue.put((data, class_name))
+        elif class_name == "SensorAgent":
+            for to_id in self.sensor_id_to_ids_dict[data.id]:
+                self.task_supervisor.sensor_dispatcher.put_data(to_id, data)
 
-    def create(data):
-        pass
+    def create(self, entity):
+        class_name = str(entity.__class__.__name__)
 
-    def update(data):
-        pass
+        if class_name == "TaskSupervisorInfo":
+            for interface in self.interfaces.values():
+                interface.create(entity)
+        else:
+            interface = self.get_interface_by_broker_id(entity.broker_ref_id)
+            interface.create(entity)
 
-    def delete(data):
-        pass
+    def update(self, entity):
+        class_name = str(entity.__class__.__name__)
+
+        if class_name == "TaskSupervisorInfo":
+            for interface in self.interfaces.values():
+                interface.update(entity)
+        else:
+            interface = self.get_interface_by_broker_id(entity.broker_ref_id)
+            interface.update(entity)
+
+    def delete(self, id, broker_id, delete_entity=True):
+        interface = self.get_interface_by_broker_id(broker_id)
+        interface.delete(id, delete_entity=delete_entity)
+
+    def get_interface_by_broker_id(self, broker_id):
+        for interface in self.interfaces.values():
+            if interface.broker_id == broker_id:
+                return interface
+        raise ValueError("Unknown BrokerID: {}".format(broker_id))
+    
+    def shutdown(self):
+        for interface in self.interfaces.values():
+            interface.shutdown()
--- tasksupervisor/endpoint/fiware_orion/orion_interface.py
+++ tasksupervisor/endpoint/fiware_orion/orion_interface.py
@@ -1,34 +1,47 @@
 import threading
-from threading import Thread
 import json
 import logging
-import tasksupervisor.my_globals as my_globals 
 
 # import local libs
-from tasksupervisor.endpoint.broker_interface import BrokerInterface
-from tasksupervisor.flask_setup import create_flask_app
+import tasksupervisor.my_globals as my_globals
+
+from tasksupervisor.helpers import servercheck
 from tasksupervisor.helpers.config_reader import ConfigReader
-from tasksupervisor.api.materialflow import Materialflow
-from tasksupervisor.endpoint.fiware_orion.entities import materialflow
+
+from tasksupervisor.flask_setup import create_flask_app
+
+from tasksupervisor.endpoint.fiware_orion.entities.materialflow import Materialflow
+from tasksupervisor.endpoint.fiware_orion.entities.sensor_agent_node import SensorAgent
+from tasksupervisor.endpoint.fiware_orion.entities.materialflow_specification_state import MaterialflowSpecificationState
+from tasksupervisor.endpoint.fiware_orion.entities.transport_order_update import TransportOrderUpdate
+from tasksupervisor.endpoint.fiware_orion.entities.tasksupervisor_info import TaskSupervisorInfo
+from tasksupervisor.endpoint.fiware_orion.entities.materialflow_update import MaterialflowUpdate
+
+from tasksupervisor.endpoint.broker_interface import BrokerInterface
 from tasksupervisor.endpoint.fiware_orion.contextbrokerhandler import ContextBrokerHandler
 
-from tasksupervisor.endpoint.broker_connector import BrokerConnector
-from tasksupervisor.task_supervisor_knowledge import TaskSupervisorKnowledge
-from tasksupervisor.__main__ import callback_new_materialflow
-
-
 logger = logging.getLogger(__name__)
+
+def callback_flask_server(flask_app):
+    """ Callback method to create for the flask server """
+    logger.info("Starting thread_flask_server")
+    flask_app.run(host=my_globals.parsed_config_file.FLASK_HOST,
+                  port=my_globals.parsed_config_file.TASKPLANNER_PORT,
+                  threaded=True, use_reloader=False, debug=True)
 
 class OrionInterface(BrokerInterface):
     """ Implements the BrokerInterface for the Orion Context Broker """
 
-    lock = threading.Lock()
+    def __init__(self, tasksupervisor_knowledge, broker_name = ""):
+        BrokerInterface.__init__(self)
 
-    def __init__(self, tasksupervisor_knowledge):
-        threading.Thread.__init__(self)
         self.tasksupervisor_knowledge = tasksupervisor_knowledge
         self.subscription_dict = {}
         self.broker_connector = tasksupervisor_knowledge.broker_connector
+
+        self.flask_app = create_flask_app(tasksupervisor_knowledge, self)
+
+        self.lock = threading.Lock()
 
         config_file_path = "./tasksupervisor/fiware_config.ini"
 
@@ -39,66 +52,92 @@ class OrionInterface(BrokerInterface):
             raise Exception("Error while parsing Fiware config file")
         
         self.context_broker_handler = ContextBrokerHandler(parsed_config_file.get_fiware_server_address())
-        
-        self.thread_new_materialflow = Thread(target=callback_new_materialflow,
-                                              args=(my_globals.taskQueue, tasksupervisor_knowledge))
-        self.app = create_flask_app(tasksupervisor_knowledge, self)
-    
-    def start_interface(self):
-        self.thread_new_materialflow.start()
-        self.start()
 
-    def run(self):
-        self.app.run(host=my_globals.parsed_config_file.FLASK_HOST,
-                port=my_globals.parsed_config_file.TASKPLANNER_PORT,
-                threaded=True, use_reloader=False, debug=True)
+        logger.info("Setting up thread_check_if_server_is_up")
+        self.thread_check_if_server_is_up = threading.Thread(name='checkServerRunning',
+                                                             target=servercheck.webserver_is_running,
+                                                             args=("localhost", my_globals.parsed_config_file.TASKPLANNER_PORT,))
+
+        logger.info("Setting up thread_flask_server")
+        self.thread_flask_server = threading.Thread(name='callback_flask_server', target=callback_flask_server, args=(self.flask_app,))
+
+    def start_interface(self):
+        self.thread_check_if_server_is_up.start()
+        self.thread_flask_server.start()
+        logger.info("Starting Flask and wait")
+        self.thread_check_if_server_is_up.join()
+        logger.info("Flask is running")
 
     def subscribe(self, topic, opt_data=None, generic=False):
-        class_name = str(topic.__class__.__name__)
-        description = class_name + " subscription"
-        if opt_data:
-            description = opt_data.description
-        notification = my_globals.parsed_config_file.get_taskplanner_address() + "/" + class_name.lower()
-        entities = [{"id": topic.id, "type": class_name}]
-        sub_id = self.context_broker_handler.subscribe_to_entity(description, entities,
-                                                                 notification, generic=generic)
-        self.subscription_dict[sub_id] = class_name
+        with self.lock:
+            class_name = str(topic.__class__.__name__)
+            description = class_name + " subscription"
+            notification = my_globals.parsed_config_file.get_taskplanner_address() + "/" + class_name.lower()
+
+            if opt_data:
+                description = opt_data.description
+                if class_name == "SensorAgent":
+                    notification = my_globals.parsed_config_file.get_taskplanner_address() + "/san/" + opt_data.to_id
+                
+            entities = [{"id": topic.id, "type": class_name}]
+            sub_id = self.context_broker_handler.subscribe_to_entity(description, entities,
+                                                                     notification, generic=generic)
+            self.subscription_dict[sub_id] = class_name
+            return sub_id
 
     def create(self, entity):
-        pass
+        fiware_entity = self.create_fiware_entity(entity)
+        self.context_broker_handler.create_entity(fiware_entity)
 
     def update(self, entity):
-        pass
+        fiware_entity = self.create_fiware_entity(entity)
+        fiware_entity.update_time()
+        self.context_broker_handler.update_entity(fiware_entity)
 
-    def delete(self, entity_id):
-        pass
+    def delete(self, id, delete_entity=True):
+        with self.lock:
+            if delete_entity:
+                self.context_broker_handler.delete_entity(id)
+            else:
+                self.context_broker_handler.delete_subscription_by_id(id)
+
+    def create_fiware_entity(self, entity):
+        class_name = str(entity.__class__.__name__)
+        fiware_entity = None
+
+        if class_name == "MaterialflowSpecificationState":
+            fiware_entity = MaterialflowSpecificationState.from_api_object(entity)  
+        elif class_name == "MaterialflowUpdate":
+            fiware_entity = MaterialflowUpdate.from_api_object(entity) 
+        elif class_name == "TaskSupervisorInfo":
+            fiware_entity = TaskSupervisorInfo.from_api_object(entity) 
+        elif class_name == "TransportOrderUpdate":
+            fiware_entity =  TransportOrderUpdate.from_api_object(entity)
+        else:
+            raise ValueError("Creation of fiware entity for unknown class was requested: {}".format(class_name))
+
+        return fiware_entity
 
     def retreive(self, json_requests):
-        subscription_id = json_requests[my_globals.FI_SUB_ID]
-        entity_type = self.subscription_dict[subscription_id]
-        if entity_type == "Materialflow":
-            # it might be possible that there are multiple entities
-            # iterate over each json request
-            for temp_json_request in json_requests[my_globals.FI_DATA]:
-                # create an entity from the json request
-                orion_materialflow = materialflow.Materialflow.CreateObjectFromJson(
-                    temp_json_request)
+        with self.lock:
+            subscription_id = json_requests[my_globals.FI_SUB_ID]
+            entity_type = self.subscription_dict[subscription_id]
+            if entity_type == "Materialflow":
+                # it might be possible that there are multiple entities
+                # iterate over each json request
+                for temp_json_request in json_requests[my_globals.FI_DATA]:
+                    # create an entity from the json request
+                    orion_materialflow = Materialflow.CreateObjectFromJson(temp_json_request)
 
-                api_materialflow = orion_materialflow.to_api_object()
-                self.broker_connector.retreive(api_materialflow, self)
+                    api_materialflow = orion_materialflow.to_api_object()
+                    api_materialflow.broker_ref_id = self.broker_id
+                    self.broker_connector.retreive(api_materialflow, self)
+            elif entity_type == "SensorAgent":
+                for temp_json_request in json_requests[my_globals.FI_DATA]:
+                    orion_sensor_agent = SensorAgent.CreateObjectFromJson(temp_json_request)
+                    
+                    api_sensor_agent = orion_sensor_agent.to_api_object()
+                    self.broker_connector.retreive(api_sensor_agent, self)
 
     def shutdown(self):
-        pass
-
-if __name__ == "__main__":
-    broker_connector = BrokerConnector()
-    task_supervisor = TaskSupervisorKnowledge()
-    task_supervisor.broker_connector = broker_connector
-
-    interface = OrionInterface(task_supervisor)
-
-    broker_connector.register_interface(interface)
-    interface.start_interface()
-
-    new_materialflow = Materialflow()
-    broker_connector.subscribe_generic(new_materialflow)
+        self.context_broker_handler.shutdown()
